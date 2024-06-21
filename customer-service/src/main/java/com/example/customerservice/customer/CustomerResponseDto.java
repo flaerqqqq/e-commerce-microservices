065e1405c0bc126431904ab2f1ee34fc0228fd85
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-21 18:19:11 +0300

    Created dto for update request

--- customer-service/src/main/java/com/example/customerservice/customer/CustomerResponseDto.java
+++ customer-service/src/main/java/com/example/customerservice/customer/CustomerResponseDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RegisterResponseDto {
+public class CustomerResponseDto {
 
     private String id;
 

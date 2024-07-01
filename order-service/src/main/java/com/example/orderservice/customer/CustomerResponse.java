afe67f70298316091b6e0aa424e2bff85f1a1023
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-01 15:54:13 +0300

    OpenFeign customer client

---
+++ order-service/src/main/java/com/example/orderservice/customer/CustomerResponse.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.customer;
+
+public class CustomerResponse {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}

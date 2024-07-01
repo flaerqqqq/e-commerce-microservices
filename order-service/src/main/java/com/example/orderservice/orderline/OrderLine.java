ada2b00d66e9bb73a66cb23a492588ac7c8f4090
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-01 14:33:52 +0300

    Created OrderLine and PaymentMethod entities

---
+++ order-service/src/main/java/com/example/orderservice/orderline/OrderLine.java
@@ -0,0 +1,28 @@
+package com.example.orderservice.orderline;
+
+import com.example.orderservice.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private double quantity;
+}
